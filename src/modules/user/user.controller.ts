997f713131bd95b2a02f5971f368e50e84ae5587
Saicharanpalagiri <charan517214@gmail.com>
2022-12-23 12:24:09 +0530

    added user first

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './User.service';
+
+
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Get()
+  getHello(): string {
+    return this.userService.getHello();
+  }
+}
